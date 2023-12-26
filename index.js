34e3ff562b60e25d918f8ba7a438221bf8701c76
Patrick Harrison <bwoysie876@gmail.com>
2023-12-26 14:18:19 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//document.getElementById('count-el').innerText = 6 
+
+let count = 0;
+console.log(count)
+let myAge = 66;
+console.log(myAge)
+let humanDogRation = (7 * 2)
+console.log(humanDogRation)
+myDogAge = myAge * humanDogRation
+console.log(myDogAge)
